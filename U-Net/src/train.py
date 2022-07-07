1a4d685dba5022daf9814f06684fba68f30716a8
Shivansh Baveja <sbaveja@mit.edu>
2022-07-07 11:30:33 -0400

    Worked on U-Net segmentation model.

---
+++ U-Net/src/train.py
@@ -0,0 +1,98 @@
+import torch
+import albumentations
+import torch.nn as nn
+import torch.optim as optim
+
+from albumentations.pytorch import ToTensorV2
+from tqdm import tqdm
+
+from model import UNET
+from utils import (
+    load_checkpoint,
+    save_checkpoint,
+    get_loaders,
+    check_accuracy,
+    save_predictions_as_imgs
+)
+
+# hyperparameters
+LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 16
+NUM_EPOCHS = 3
+NUM_WORKERS = 2
+IMAGE_HEIGHT = 160
+IMAGE_WIDTH = 240
+PIN_MEMORY = True
+LOAD_MODEL = False
+TRAIN_IMG_DIR = "../data/train_images/"
+TRAIN_MASK_DIR = "../data/train_masks/"
+VAL_IMG_DIR = "../data/val_images"
+VAL_MASK_DIR = '../data/val_masks'
+
+def train_fn(loader, model, optimizer, loss_fr, scaler):
+    loop = tqdm(loader)
+
+    for batch_idc, (data, targets) in enumerate(loop):
+        data = data.to(device=DEVICE)
+        targets = targets.float().unsqueeze(1).to(device=DEVICE)
+
+        # forward
+        with torch.cuda.amp.autocast():
+            predictions = model(data)
+            loss = loss_fn(predictions, targets)
+
+        # backward
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        loop.set_postfix(loss=loss.item())
+
+def main():
+    train_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std = [1.0, 1.0, 1.0],
+                max_pixel_value = 255.0
+            ),
+            ToTensorV2()
+        ]
+    )
+
+    val_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std = [1.0, 1.0, 1.0],
+                max_pixel_value = 255.0
+            ),
+            ToTensorV2()
+        ]
+    )
+
+    model = UNET(in_channels=3, out_channels=3).to(DEVICE)
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    train_loader, val_loader = get_loaders(
+        TRAIN_IMG_DIR,
+        TRAIN_MASK_DIR,
+        VAL_IMG_DIR,
+        VAL_MASK_DIR,
+        BATCH_SIZE,
+        train_transform
+        val_transform
+    )
+
+    scaler = torch.cuda.amp.GradScaler()
+    for epoch in range(NUM_EPOCHS):
+        train_fr(train_loader, model, optimizer, loss_fn, scaler)
+
+
+if __name__ == "__main__":
+    main()
